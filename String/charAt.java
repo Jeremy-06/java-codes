190979d1885383bf000b040a39d392c15eeedd6f
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-09-03 15:17:08 +0800

    String

---
+++ String/charAt.java
@@ -0,0 +1,7 @@
+public class charAt {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        char character = text.charAt(7);
+        System.out.println("The character at index 7 is: " + character);
+    }
+}
---
+++ String/length.java
@@ -0,0 +1,7 @@
+public class length {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        int length = text.length();
+        System.out.println("The length of the string is: " + length);
+    }
+}
