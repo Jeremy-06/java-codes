d4f74fb7e5a879a932118d1439b283e5bf18d105
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-09-21 20:36:28 +0800

    day3

---
+++ 1-java-daily/day3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class day3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("EVEN OR ODD?");
+        System.out.print("\nEnter a Number: ");
+        double num = scanner.nextDouble();
+        evenOdd(num);
+
+    }
+    static void evenOdd(double num){
+        if (num % 2 == 1 ){
+            System.out.println("The number is odd.");
+        } else {
+            System.err.println("The number is even");
+        }
+    }
+}
